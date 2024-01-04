4cc94b61811b72b027a7cc01127c1b3ccc4d9e69
Samticode <samti8686@gmail.com>
2024-01-04 17:50:37 +0100

    im the alpha im the omega everything in between

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+// ----------------------------------------------------------------
+
+const mainFolder = path.join(__dirname, 'public', 'oss');
+const tilbakemeldingFolder = path.join(__dirname, 'public', 'tilbakemeldinger');
+const produktFolder = path.join(__dirname, 'public', 'produkter');
+
+app.use('/', express.static(mainFolder));
+app.use('/produkter', express.static(produktFolder));
+app.use('/tilbakemeldinger', express.static(tilbakemeldingFolder));
+
+// ----------------------------------------------------------------
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(mainFolder, 'index.html'));
+});
+app.get('/tilbakemeldinger', (req, res) => {
+    res.sendFile(path.join(tilbakemeldingFolder, 'index.html'));
+});
+app.get('/produkter', (req, res) => {
+    res.sendFile(path.join(produktFolder, 'index.html'));
+});
+
+// ----------------------------------------------------------------
+
+const PORT = 3000 || process.env.PORT;
+app.listen(PORT);
