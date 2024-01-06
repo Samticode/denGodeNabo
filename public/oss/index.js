d278918901b98c4e38209a5b7b5bb84be959979b
Samticode <samti8686@gmail.com>
2024-01-06 20:35:11 +0100

    @

--- public/oss/index.js
+++ public/oss/index.js
@@ -1,1 +1,11 @@
-console.log('oss');
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', (e) => {
+    if (window.scrollY > 120) {
+        navbar.style.backgroundColor = 'var(--navbar-seethrough)';
+        navbar.style.borderBottom = '0.5px solid var(--black';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.borderBottom = 'none';
+    }
+});
