8f015ffc7d288441a29aa1730e1c4d57dd5165b2
Samticode <samti8686@gmail.com>
2024-01-09 20:10:58 +0100

    rondom commit

--- public/oss/index.js
+++ public/oss/index.js
@@ -4,8 +4,11 @@ window.addEventListener('scroll', (e) => {
     if (window.scrollY > 120) {
         navbar.style.backgroundColor = 'var(--navbar-seethrough)';
         navbar.style.borderBottom = '0.5px solid var(--black';
+        // navbar.style.mixBlendMode = 'difference';
     } else {
         navbar.style.backgroundColor = 'transparent';
         navbar.style.borderBottom = 'none';
     }
 });
+
+
