c9783f167136c4b2ebda3a4b60ac66ecfd02d238
Samticode <samti8686@gmail.com>
2024-01-14 19:13:54 +0100

    glemte å gjøre i går

--- public/produkter/index.js
+++ public/produkter/index.js
@@ -1,1 +1,14 @@
-console.log('produkter');
+const navbar = document.getElementById('navbar');
+
+window.addEventListener('scroll', (e) => {
+    if (window.scrollY > 120) {
+        navbar.style.backgroundColor = 'var(--navbar-seethrough)';
+        navbar.style.borderBottom = '0.5px solid var(--black';
+        // navbar.style.mixBlendMode = 'difference';
+    } else {
+        navbar.style.backgroundColor = 'transparent';
+        navbar.style.borderBottom = 'none';
+    }
+});
+
+
