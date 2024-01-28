06d0c2317adb0d68f67a24f5ff23a5fa1c7597d3
Samticode <samti8686@gmail.com>
2024-01-28 19:03:07 +0100

    fixes

--- app.js
+++ app.js
@@ -27,5 +27,5 @@ app.get('/produkter', (req, res) => {
 
 // ----------------------------------------------------------------
 
-const PORT = 3000 || process.env.PORT;
-app.listen(PORT);
+const port = process.env.PORT || 3000;
+app.listen(port);
